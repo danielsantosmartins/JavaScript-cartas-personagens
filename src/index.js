38149fe21f2a451edcef2bc32d18e41a1fac5176
Daniel Dos Santos Martins <danielsantosmartins.dev@gmail.com>
2023-07-20 22:11:58 -0300

    projeto OO finalizado

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-const personagemPedrinho = {
-    nome: 'Pedrinho',
-    vida: 7,
-    mana: 12,
-    level: 5,
-    tipo: 'Mago',
-}
+import { PersonagemView } from "./components/personagem-view.js";
+import { ArqueiroMago } from "./modules/arqueiro-mago.js";
+import { Arqueiro } from "./modules/arqueiro.js";
+import { Guerreiro } from "./modules/guerreiro.js";
+import { Mago } from "./modules/mago.js";
+import { Personagem } from "./modules/personagem.js";
 
-const personagemJose = {
-    nome: 'Jose',
-    vida: 7,
-    mana: 6,
-    level: 3,
-    tipo: 'Arqueiro',
-}
+const magoAntonio = new Mago("Antonio", 4, "fogo", 4, 3);
+const magaJulia = new Mago("Julia", 8, "gelo", 7, 10);
+const arqueiroBruno = new Arqueiro("Bruno", 7, 8);
+const arqueiroMagoChico = new ArqueiroMago("Chico", 7, 10, "ar", 4, 8);
+const guerreiraJorge = new Guerreiro("Jorge", 8);
 
+const personagens = [
+  magoAntonio,
+  magaJulia,
+  arqueiroBruno,
+  arqueiroMagoChico,
+  guerreiraJorge,
+];
+
+new PersonagemView(personagens).render();
+
+/* console.log(Personagem.verificarVencedor(arqueiroBruno, magoAntonio)); */
+console.log(arqueiroBruno);
